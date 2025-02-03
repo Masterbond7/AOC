f9f584c97a9e0bbea56edb8daa77e020eb21d729
Masterbond7 <jamesslough4@gmail.com>
2025-02-03 19:09:34 +1300

    mmm stat struct is 144 bytes

--- 2024/day1/code.asm
+++ 2024/day1/code.asm
@@ -1,20 +1,37 @@
 global _start
 
 section .data
-    hello db "Hello world!", 0x0A
-    hello_len equ $ - hello
+    ; Path and length for input file
+    path db "./input", 0x00
+    path_len equ $ - path
 
 section .bss
+    output resb 144
+    size resb 8
 
 section .text
 ; Main function
 _start:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, hello
-    mov rdx, hello_len
+    ; Open input file
+    mov rax, 2    ; sys_open
+    mov rdi, path ; Filename pointer
+    mov rsi, 0    ; Flags (O_RDONLY)
+    syscall
+    push rax ; Push file descriptor onto stack
+
+    ; Get file status and size
+    mov rax, 5           ; sys_fstat
+    mov rdi, qword [rsp] ; Move the 64bit num from stack to RDI
+    mov rsi, output      ; Give pointer to output for the return value
+    syscall
+    mov rax, qword [output + 0x30] ; Get file size
+
+    ; Close input file
+    mov rax, 3 ; close(2)
+    pop rdi ; Pop fd from stack into fd parameter
     syscall
 
+    ; Exit program
     mov rax, 60
     mov rdi, 0
     syscall
