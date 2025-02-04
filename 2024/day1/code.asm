0a60f100760866b8bd28ad2a40dfd0052e9de180
Masterbond7 <jamesslough4@gmail.com>
2025-02-05 00:38:46 +1300

    Base pointer! (my variables are no longer running away) :D

--- 2024/day1/code.asm
+++ 2024/day1/code.asm
@@ -10,6 +10,9 @@ section .bss
 section .text
 ; Main function
 _start:
+    ; Init rbp
+    mov rbp, rsp
+
     ; Open input file
     mov rax, 2    ; sys_open
     mov rdi, path ; Filename pointer
@@ -19,14 +22,14 @@ _start:
     cmp rax, 0    ; If error (<0)
     jl error_exit ; Jump to error_exit
 
-    push rax      ; Otherwise push file descriptor onto stack
+    push rax      ; Otherwise push file descriptor onto stack (rbp-8)
 
 
     ; Get file status and size
-    mov rax, 5           ; sys_fstat
-    mov rdi, qword [rsp] ; Copy the fd from stack to RDI
-    sub rsp, 144         ; Allocate 144 bits on the stack for return struct
-    mov rsi, rsp         ; Return structure to the stack
+    mov rax, 5             ; sys_fstat
+    mov rdi, qword [rbp-8] ; Copy the fd from stack to RDI
+    sub rsp, 144           ; Allocate 144 bits on the stack for return struct
+    mov rsi, rsp           ; Return structure to the stack
     syscall
 
     cmp rax, 0    ; If error (<0)
@@ -34,31 +37,31 @@ _start:
 
     mov rax, qword [rsp + 0x30] ; Otherwise, get file size
     add rsp, 136                ; Move the stack pointer back - 8 bytes (144-8)
-    mov [rsp], rax              ; Move size from rax to stack
+    mov [rsp], rax           ; Move size from rax to stack (rbp-16)
 
 
     ; Map some memory and load the file!
-    mov rax, 9      ; sys_mmap
-    mov rdi, 0      ; Set address to 0 (let the computer choose)
-    mov rsi, [rsp]  ; Set the length to the file size (from stack)
-    mov rdx, 0x01   ; Set protection to PROT_READ
-    mov r10, 0x02   ; Set flags to MAP_PRIVATE
-    mov r8, [rsp+8] ; Set the fd (from stack)
-    mov r9, 0       ; Set the offset to 0
+    mov rax, 9        ; sys_mmap
+    mov rdi, 0        ; Set address to 0 (let the computer choose)
+    mov rsi, [rbp-16] ; Set the length to the file size (from stack)
+    mov rdx, 0x01     ; Set protection to PROT_READ
+    mov r10, 0x02     ; Set flags to MAP_PRIVATE
+    mov r8, [rbp-8]   ; Set the fd (from stack)
+    mov r9, 0         ; Set the offset to 0
     syscall
 
     cmp rax, 0    ; If error (<0)
     jl error_exit ; Jump to error_exit
 
     sub rsp, 8     ; Allocate 8 bytes on stack
-    mov [rsp], rax ; Move file pointer to stack
+    mov [rsp], rax ; Move file pointer to stack (rbp-24)
 
 
     ; Calculate list size required (RAX)
-    mov rax, [rsp+8] ; Load file size into RAX
-    mov rdx, 0       ; Set RDX to 0
-    mov rbx, 14      ; 14 bytes per line
-    div rbx          ; Divide to calculate no. lines (/numbers)
+    mov rax, [rbp-16] ; Load file size into RAX
+    mov rdx, 0        ; Set RDX to 0
+    mov rbx, 14       ; 14 bytes per line
+    div rbx           ; Divide to calculate no. lines (/numbers)
 
     mov rdx, 0 ; Set RDX to 0
     mov rbx, 4 ; 4 bytes per number (11111-99999 so 32-bit)
@@ -66,9 +69,9 @@ _start:
 
 
     ; Unmap the memory for the input file
-    mov rax, 11      ; sys_munmap
-    mov rdi, [rsp]   ; Set addr to file pointer (from stack)
-    mov rsi, [rsp+8] ; Set len to the file size (from stack)
+    mov rax, 11       ; sys_munmap
+    mov rdi, [rbp-24] ; Set addr to file pointer (from stack)
+    mov rsi, [rbp-16] ; Set len to the file size (from stack)
     syscall
 
     cmp rax, 0    ; If error (<0)
