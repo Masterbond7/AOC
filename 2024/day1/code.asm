b7156991f5a760c12bf4679e8f6b6f6e2c9fdfa9
Masterbond7 <jamesslough4@gmail.com>
2025-02-04 22:14:59 +1300

    Error handling :D

--- 2024/day1/code.asm
+++ 2024/day1/code.asm
@@ -15,27 +15,50 @@ _start:
     mov rdi, path ; Filename pointer
     mov rsi, 0    ; Flags (O_RDONLY)
     syscall
-    push rax ; Push file descriptor onto stack
+
+    cmp rax, 0    ; If error (<0)
+    jl error_exit ; Jump to error_exit
+
+    push rax      ; Otherwise push file descriptor onto stack
+
 
     ; Get file status and size
     mov rax, 5           ; sys_fstat
-    mov rdi, qword [rsp] ; Move the 64bit num from stack to RDI
-    sub rsp, 144         ; Move stack pointer 144 bits for return struct
+    mov rdi, qword [rsp] ; Move the fd from stack to RDI
+    sub rsp, 144         ; Allocate 144 bits on the stack for return struct
     mov rsi, rsp         ; Return structure to the stack
     syscall
-    mov rax, qword [rsp + 0x30] ; Get file size
+
+    cmp rax, 0    ; If error (<0)
+    jl error_exit ; Jump to error_exit
+
+    mov rax, qword [rsp + 0x30] ; Otherwise, get file size
     add rsp, 136                ; Move the stack pointer back - 8 bytes (144-8)
-    mov [rsp], rax             ; Move size from rax to [size] var
+    mov [rsp], rax              ; Move size from rax to stack
+
 
     ; Get rid of file size
     add rsp, 8 ; Move back 8 bytes
 
+
     ; Close input file
-    mov rax, 3 ; close(2)
-    pop rdi ; Pop fd from stack into fd parameter
+    mov rax, 3 ; sys_close
+    pop rdi    ; Pop fd from stack into fd parameter
     syscall
+
+    cmp rax, 0    ; If error (<0)
+    jl error_exit ; Jump to error_exit
+
 
     ; Exit program
     mov rax, 60
     mov rdi, 0
+    syscall
+
+
+error_exit:
+    ; Exit with errno code
+    mov rdi, rax ; Move return (error) value to rdi
+    neg rdi      ; Make it positive
+    mov rax, 60  ; sys_exit
     syscall
